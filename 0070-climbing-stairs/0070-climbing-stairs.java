570710fc682cb090451f5b325becb6fc1e01ada5
Pavithran S <s.pavithran.comp2020@gmail.com>
2025-03-10 23:42:53 +0530

    Time: 0 ms (100%), Space: 40.6 MB (37.72%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+
+        int first = 1, second = 2;
+        for (int i = 3; i <= n; i++){
+            int temp = first + second;
+            first =  second;
+            second = temp;
+        }
+        return second;
+    }
+}
