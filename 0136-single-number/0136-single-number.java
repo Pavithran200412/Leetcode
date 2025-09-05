45339556ff05667482c558040539e8e0f470c928
Pavithran S <s.pavithran.comp2020@gmail.com>
2025-09-05 18:59:44 +0530

    Time: 355 ms (6.46%), Space: 46.9 MB (5.68%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int count = 0;
+        int num = 0;
+        for(int i = 0; i < nums.length; i++){
+            num = nums[i];
+            count = 0;
+            for(int j = 0; j < nums.length; j++){
+                if(nums[j] == num)
+                count++;
+            }
+            if(count == 1)
+            return num;
+        }
+        return num;
+    }
+}
