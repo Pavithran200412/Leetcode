474da4fd7c29c362e452e830c586d690786bb8f7
Pavithran S <s.pavithran.comp2020@gmail.com>
2025-09-11 23:12:49 +0530

    Time: 10 ms (13.41%), Space: 78.8 MB (27.82%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        ArrayList<Integer> p = new ArrayList<>();
-        ArrayList<Integer> n = new ArrayList<>();
+        ArrayList<Integer> pid = new ArrayList<>();
+        ArrayList<Integer> nid = new ArrayList<>();
 
         for(int i = 0; i < nums.length; i++){
             if(nums[i] > 0){
-                p.add(nums[i]);
+                pid.add(nums[i]);
             }else{
-                n.add(nums[i]);
+                nid.add(nums[i]);
             }
         }
         for(int i = 0; i < nums.length / 2; i++){
-            nums[2 * i] = p.get(i);
-            nums[2*i + 1] = n.get(i);
+            nums[2 * i] = pid.get(i);
+            nums[2*i + 1] = nid.get(i);
         }
         return nums;
     }
