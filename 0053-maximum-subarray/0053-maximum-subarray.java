5d9dc9872a3c5cb7194f5d90d1be92d0972fee74
Pavithran S <s.pavithran.comp2020@gmail.com>
2025-09-14 14:42:54 +0530

    Time: 1 ms (99.49%), Space: 57 MB (70.77%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,26 +1,38 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum = 0;
-        int sums = Integer.MIN_VALUE;
+        // int sum = 0;
+        // int sums = Integer.MIN_VALUE;
 
-        int start = 0;
-        int ansStart = -1, ansEnd = -1;
+        // int start = 0;
+        // int ansStart = -1, ansEnd = -1;
 
-        for(int i = 0; i < nums.length; i++){
-            if (sum == 0) start = i;
+        // for(int i = 0; i < nums.length; i++){
+        //     if (sum == 0) start = i;
 
-            sum += nums[i];
+        //     sum += nums[i];
 
-            if(sum > sums){
-                sums = sum;
+        //     if(sum > sums){
+        //         sums = sum;
 
-                ansStart = start;
-                ansEnd = i;
-            }
-            if(sum < 0){
-                sum = 0;
-            }
+        //         ansStart = start;
+        //         ansEnd = i;
+        //     }
+        //     if(sum < 0){
+        //         sum = 0;
+        //     }
+        // }
+        // return sums;
+
+        int currentSum = nums[0]; // Start with the first element
+        int maxSum = nums[0];     // Initialize maxSum with the first element
+
+        // Traverse the array from the second element
+        for (int i = 1; i < nums.length; i++) {
+            // If currentSum is negative, reset to current element
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            // Update maxSum if currentSum is greater
+            maxSum = Math.max(maxSum, currentSum);
         }
-        return sums;
+        return maxSum;
     }
 }
