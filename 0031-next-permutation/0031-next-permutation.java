aff311ccef45303a39fa25e5820dcd953100b3ab
Pavithran S <s.pavithran.comp2020@gmail.com>
2025-09-19 20:58:44 +0530

    Time: 0 ms (100%), Space: 43.6 MB (6.02%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -16,13 +16,13 @@ class Solution {
         
         reverse(nums, i + 1, n - 1);
     }
-    private void swap(int[] nums, int i, int j) {
+    public void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
     
-    private void reverse(int[] nums, int i, int j) {
+    public void reverse(int[] nums, int i, int j) {
         while (i < j) {
             swap(nums, i, j);
             i++;
