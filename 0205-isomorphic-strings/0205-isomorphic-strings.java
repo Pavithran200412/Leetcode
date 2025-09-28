aa12310bd246a801e401b33169b97c9be478910d
Pavithran S <s.pavithran.comp2020@gmail.com>
2025-09-28 14:27:26 +0530

    Time: 6 ms (88.58%), Space: 42.6 MB (53.96%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,12 +1,26 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-          int[] m1 = new int[256], m2 = new int[256];
-          int n = s.length();
-          for (int i = 0; i < n; ++i) {
-              if (m1[s.charAt(i)] != m2[t.charAt(i)]) return false;
-              m1[s.charAt(i)] = i + 1;
-              m2[t.charAt(i)] = i + 1;
-          }
-          return true;
+        if (s.length() != t.length()) return false;
+        int[] sToT = new int[256];
+        int[] tToS = new int[256];
+
+        for (int i = 0; i < 256; i++) {
+            sToT[i] = -1;
+            tToS[i] = -1;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (sToT[c1] != -1 && sToT[c1] != c2) return false;
+            if (tToS[c2] != -1 && tToS[c2] != c1) return false;
+
+            // Add mapping
+            sToT[c1] = c2;
+            tToS[c2] = c1;
+        }
+
+        return true;
     }
 }
