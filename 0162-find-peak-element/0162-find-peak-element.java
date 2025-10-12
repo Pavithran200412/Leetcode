024641528540e1cda7336bf1dbad3f7d859768e4
Pavithran S <s.pavithran.comp2020@gmail.com>
2025-10-12 18:07:31 +0530

    Time: 0 ms (100%), Space: 42.9 MB (13.64%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -6,13 +6,11 @@ class Solution {
             int mid = left + (right - left) / 2;
             
             if (nums[mid] > nums[mid + 1]) {
-                // Peak lies on the left side (including mid)
                 right = mid;
             } else {
-                // Peak lies on the right side
                 left = mid + 1;
             }
         }
-        return left; // left == right -> peak index
+        return left;
     }
 }
