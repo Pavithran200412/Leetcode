42644685a02fedfbdc98f374c3aa471307e574b7
Pavithran S <s.pavithran.comp2020@gmail.com>
2025-10-20 17:43:26 +0530

    Time: 5 ms (99.42%), Space: 46 MB (74.52%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -3,15 +3,13 @@ class Solution {
         int row = 0, col = matrix[0].length - 1;
 
         while (row < matrix.length && col >= 0) {
-            int val = matrix[row][col];
-            if (val == target)
+            if (matrix[row][col] == target)
                 return true;
-            else if (val > target)
+            else if (matrix[row][col] > target)
                 col--;
             else
                 row++;
         }
-
         return false;
     }
 }
