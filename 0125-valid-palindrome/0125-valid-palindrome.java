01625c6c007c163025a34c44d6f1bd76f1a2d3e6
Pavithran S <s.pavithran.comp2020@gmail.com>
2026-05-26 11:31:05 +0530

    Time: 13 ms (36.05%), Space: 46.4 MB (27.2%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int start = 0, end = s.length() -1;
-        while(start < end){
-            char l = s.charAt(start), r = s.charAt(end);
-            if(!Character.isLetterOrDigit(l)){
-                start++;
-            }else if(!Character.isLetterOrDigit(r)){
-                end--;
-            }else if(Character.toLowerCase(l) != Character.toLowerCase(r)){
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int l = 0;
+        int r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
                 return false;
-            }else{
-            start++;
-            end--;
             }
+            l++;
+            r--;
         }
         return true;
     }
