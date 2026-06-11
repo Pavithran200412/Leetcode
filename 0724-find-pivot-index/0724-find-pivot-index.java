37509b73caa9045abed81c7740dab497514de999
Pavithran S <s.pavithran.comp2020@gmail.com>
2026-06-11 22:28:05 +0530

    Time: 1 ms (98.43%), Space: 47.7 MB (8.69%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,21 +1,5 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        // int n = nums.length;
-
-        // for(int i = 0; i < n; i++){
-        //     int lsum = 0;
-        //     for(int j = 0; j < i; j++){
-        //         lsum += nums[j];
-        //     }
-        //     int rsum = 0;
-        //     for(int k = i + 1; k < n; k++){
-        //         rsum += nums[k];
-        //     }
-        //     if(lsum == rsum){
-        //         return i;
-        //     }
-        // }
-        // return -1;
 
         int tsum = 0;
         for(int num : nums){
